a2b02bf9dd2e6198fa5c087223a8b245b786acfb
Jarrett Atkinson <jj_atkinson@att.net>
2015-12-22 09:52:23 -0600

    Add MeasureBot

--- src/main/groovy/com/ppcgse/koth/antichess/Main.groovy
+++ src/main/groovy/com/ppcgse/koth/antichess/Main.groovy
@@ -1,17 +1,15 @@
 package com.ppcgse.koth.antichess
 
-import com.ppcgse.koth.antichess.controller.Game
-import com.ppcgse.koth.antichess.player.MyBot
-import com.ppcgse.koth.antichess.player.OnePlayBot
+import com.ppcgse.koth.antichess.controller.Runner
 
 /**
  * Created by Jarrett on 12/15/15.
  */
 public class Main {
     public static void main(String[] args) {
-//        println 'Running'
-//        new Runner().runGames();
+        println 'Running'
+        new Runner().runGames();
 
-        new Game(new OnePlayBot(), new MyBot()).run()
+//        new Game(new OnePlayBot(), new MyBot()).run()
     }
 }
